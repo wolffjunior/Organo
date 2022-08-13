fdf20407c50f362e02d5845f32052bb77d49c817
wolff lobo <wolfflobo2@gmail.com>
2022-08-13 11:24:07 -0300

    add text-field component and minor fixs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from './components/Banner/Banner';
+import TextField from './components/TextField/TextField';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <TextField />
+    </div>
+  );
+}
+
+export default App;
