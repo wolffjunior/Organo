a2b05c19460d4cff5f71169d623bbed794ad351e
wolff lobo <wolfflobo2@gmail.com>
2022-08-13 10:47:51 -0300

    add component banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/images/banner.png" alt="O banner principal da página do Organograma"></img>
+        </header>
+        
+    )
+}
+
+export default Banner
